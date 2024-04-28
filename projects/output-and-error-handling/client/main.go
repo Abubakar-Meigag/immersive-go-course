963ee3e8f1cc76611da586a4eba6439c02710d83
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-04-28 17:54:15 +0100

    done with handle error

--- projects/output-and-error-handling/client/main.go
+++ projects/output-and-error-handling/client/main.go
@@ -7,22 +7,35 @@ import (
 )
 
 func main() {
-
 	res, err := http.Get("http://localhost:8080")
 	if err != nil {
-		panic(err)
+
+		if res != nil {
+			fmt.Println("Response status:", res.Status)
+			if res.StatusCode == http.StatusTooManyRequests {
+				fmt.Println("Server is too busy. Retry after:", res.Header.Get("Retry-After"))
+			} else {
+				fmt.Println("Failed to get response from server:", err)
+			}
+		} else {
+			fmt.Println("Failed to connect to server:", err)
+		}
+		return
 	}
 
 	defer res.Body.Close()
 
 	fmt.Println("Response status:", res.Status)
-	scanner := bufio.NewScanner(res.Body)
 
-	for i := 0; scanner.Scan() && i < 10; i++ {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
+	if res.StatusCode == http.StatusOK {
+		scanner := bufio.NewScanner(res.Body)
+		for i := 0; scanner.Scan(); i++ {
+			fmt.Println(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Println("Error response from server:", res.Status)
 	}
 }
