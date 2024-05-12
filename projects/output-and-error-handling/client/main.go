c25ba0e27ba0a85d1818e20f9d52ffc2b412014a
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-05-12 18:49:18 +0100

    update working in review and improing my previous code

--- projects/output-and-error-handling/client/main.go
+++ projects/output-and-error-handling/client/main.go
@@ -1,41 +1,65 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
+	"time"
+	"bufio"
 )
 
 func main() {
-	res, err := http.Get("http://localhost:8080")
-	if err != nil {
+	maxRetries := 3
+	retryInterval := time.Second * 2
 
-		if res != nil {
-			fmt.Println("Response status:", res.Status)
-			if res.StatusCode == http.StatusTooManyRequests {
-				fmt.Println("Server is too busy. Retry after:", res.Header.Get("Retry-After"))
+	for retry := 0; retry < maxRetries; retry++ {
+		res, err := http.Get("http://localhost:8080")
+		
+		if err != nil {
+			if res != nil {
+				fmt.Println("Response status:", res.Status)
+				if res.StatusCode == http.StatusTooManyRequests {
+					fmt.Println("Server is too busy. Retry after:", res.Header.Get("Retry-After"))
+
+					retryAfterStr := res.Header.Get("Retry-After")
+					retryAfter, err := time.ParseDuration(retryAfterStr)
+					if err != nil {
+						
+						retryAfter = retryInterval
+					}
+					time.Sleep(retryAfter)
+					continue 
+				} else {
+					fmt.Println("Failed to get response from server:", err)
+				}
 			} else {
-				fmt.Println("Failed to get response from server:", err)
+				fmt.Println("Failed to connect to server:", err)
 			}
+
+			
+			time.Sleep(retryInterval)
+			continue 
+		}
+
+		defer res.Body.Close()
+
+		fmt.Println("Response status:", res.Status)
+
+		if res.StatusCode == http.StatusOK {
+			scanner := bufio.NewScanner(res.Body)
+			for i := 0; scanner.Scan(); i++ {
+				fmt.Println(scanner.Text())
+			}
+			if err := scanner.Err(); err != nil {
+				panic(err)
+			}
+			break 
 		} else {
-			fmt.Println("Failed to connect to server:", err)
+			fmt.Println("Error response from server:", res.Status)
+			
+			time.Sleep(retryInterval)
+			continue 
 		}
-		return
 	}
 
-	defer res.Body.Close()
-
-	fmt.Println("Response status:", res.Status)
-
-	if res.StatusCode == http.StatusOK {
-		scanner := bufio.NewScanner(res.Body)
-		for i := 0; scanner.Scan(); i++ {
-			fmt.Println(scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			panic(err)
-		}
-	} else {
-		fmt.Println("Error response from server:", res.Status)
-	}
+	fmt.Println("Out of retries. Exiting...")
 }
